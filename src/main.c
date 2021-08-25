e7c80d2e3ccdde3931023013b12e9df3eb3c2a91
GiovaniZan <55863759+GiovaniZan@users.noreply.github.com>
2021-08-25 01:25:55 -0300

    Segundo commit
    Reestruturação do código - uma string por argumento de entrada. Mais versátil.
    Falta sinalizar valores de entrada sem usar o símbolo de -. Talvez utilizar o ~, operacional apenas se for o primeiro caractere do número.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,23 @@
 // main.c
 // Desenvolvido para a placa EK-TM4C1294XL
-// Verifica se recebeu alguma coisa pela serial e acende um dos leds de acordo com o caractere recebido
-// Verifica o estado da chave USR_SW2, e, caso seja pressionada envia o caracter A pela serial
+// Algumas rotinas foram descaradamente copiadas do projeto
+// uart_c
 // Prof. Guilherme Peron / Prof. Rafael de Góes
+// 
+//  
+//  Calculadora Básica
+// Os valores são inseridos via porta serial com baud de 9600, 8bits,
+// sem paridade e 1 stop bit
+//
+//
+//
 
 #include <stdint.h>
 #include "tm4c1294ncpdt.h"
-#define BUFFERSIZE 32
-#define DIGITOSENTRADA 8
+// para suportar valores de 64 bits, com finalizador de string arrays de caracteres possuem 20 posições
+#define BUFFERSIZE 20
+// Seleciona valores de entrada de 4 algarismos
+#define DIGITOSENTRADA ( 4 -  1)
 
 
 void GPIO_Init(void);
@@ -17,31 +27,38 @@ void Uart0_Init(void);
 uint8_t Uart0_Rcv(void);
 void Uart0_Send(uint8_t dado);
 
-uint8_t errostring[] = "Erro" ; 
-uint8_t buffer[BUFFERSIZE] = {0};
-uint8_t outputbuffer[BUFFERSIZE] = {0};
-uint8_t n; // posição do ultimo caractere lido
-uint8_t n_op;
-uint8_t n_fim; // posicao do final da string
+const uint8_t errostr[] = "Erro " ;  
+
+uint8_t argAstring[BUFFERSIZE] = {0}; // primeiro argumento
+uint8_t argBstring[BUFFERSIZE] = {0};  // segundo argumento
+uint8_t outputbuffer[BUFFERSIZE] = {0}; // resposta/mensagem de erro
+uint16_t caractere; 
+uint16_t n; // posição do ultimo caractere lido
+uint16_t n_op;
+uint16_t n_fim; // posicao do final da string
 int32_t operandoA, operandoB;
 int64_t res;
 int32_t arg = 0; // debug
 
-typedef enum alertas_tipos {OK, DivPZero, Arg1Max, Arg2Max} alertas;
-typedef enum operacao_tipo 
-            { nulo, soma, subtracao, multiplicacao, divisao}
+typedef enum alertas_tipos {OK, NovoAlgarismo, DivPZero, ArgMax, NovoArgumento
+            ,Executa, Reinicia} alertas;
+typedef enum operacao_tipo { nulo, soma, subtracao, multiplicacao, divisao}
               operacao_enum;
-typedef enum acao_tipo { acumuladado, executa, reinicia } acao_enum;
+//typedef enum acao_tipo { acumuladado, executa, reinicia } acao_enum;
+typedef enum argumento_numero {ArgA, ArgB, ArgC} argumento_num;
+
 
 operacao_enum operacao;
-acao_enum acao;
-alertas flow_flags;
+//acao_enum acao;
+alertas flow_flag;
+argumento_num argumento;
 
-void Parsestring(uint8_t n_op, uint8_t n_fim);
 int64_t ExecucaoOperacao (operacao_enum op);
 void OutputBuffRes64bitsHL(int64_t res);
-void TransmiteOutpurBuff();
+void TransmiteOutputBuff();
 void TransmiteString(uint8_t *string );
+int32_t ParseArgString(uint8_t *argstr);
+void TransmiteConstString(const uint8_t *string );
 
 
 
@@ -52,74 +69,110 @@ int main(void)
   Uart0_Init();
   n=0; // endereço inicio do buffer
   operacao = nulo; // nenhuma operacao definida - aquisicao do primeiro operando
-  acao = acumuladado; // acumula algarismos e operadores
-  
+  argumento = ArgA;  
+  flow_flag = OK; // nenhum alerta
     while (1)
     {
        // verifica presença de algo para ler na UART
       if ((UART0_FR_R&0x0010) ==0)
       {
-         buffer[n] = ((uint8_t)(UART0_DR_R & 0xFF));
-         Uart0_Send(buffer[n]);
-         switch(buffer[n])
+         caractere = ((uint8_t)(UART0_DR_R & 0xFF));
+//         flow_flag = NovoAlgarismo;
+         switch(caractere)
          {
          case '+':
            operacao = soma;
-           n_op = n;
+           flow_flag = NovoArgumento;
            break;
          case '-':
            operacao = subtracao;
-           n_op = n;
+           flow_flag = NovoArgumento;
            break;
          case '*':
            operacao = multiplicacao;
-            n_op = n;
+           flow_flag = NovoArgumento;
            break;
          case '/':
            operacao = divisao;
-           n_op = n;
+           flow_flag = NovoArgumento;
            break;
          case '=':
-           acao = executa;
-           n_fim = n;
+           flow_flag = Executa;
            break;
          case '0' ... '9':
+           if (n > DIGITOSENTRADA) {
+              flow_flag = ArgMax;
+           } else {
+              flow_flag = NovoAlgarismo;
+           }
            break;
          default :
-           acao=reinicia;
+           flow_flag=Reinicia;
            break;
          }
-         n++;
       } // if uart
-      // 
-      
-      switch(acao)
+         
+      switch(flow_flag)
       {
-      case reinicia:
+      case NovoAlgarismo:
+        Uart0_Send(caractere);
+        switch(argumento)
+          {
+          case ArgA:
+            argAstring[n] = caractere;
+            argAstring[n+1] = 0;
+            break;
+          case ArgB:
+            argBstring[n] = caractere;
+            argBstring[n+1] = 0;
+            break;
+          default:
+            break;
+          }
+        n++;
+        flow_flag = OK;
+        break;
+        
+        case NovoArgumento:
+          n=0; // reseta contador para proximo argumento
+          Uart0_Send(caractere);
+          argumento++;
+          flow_flag = OK;
+          break;
+       
+      case ArgMax:
+        break;
+        
+        
+      case Reinicia:
           n=0; // endereço inicio do buffer
           operacao = nulo; // nenhuma operacao definida - aquisicao do primeiro operando
-          acao = acumuladado; // acumula algarismos e operadores
           Uart0_Send('\n');
           Uart0_Send('\r');
-          flow_flags = OK;
+          flow_flag = OK;
+          argumento = ArgA;
           break;
           
-      case executa:
-          Parsestring(n_op, n_fim);
+      case Executa:
+          Uart0_Send(' ');
+          operandoA = ParseArgString(argAstring);
+          Uart0_Send('=');
+          operandoB = ParseArgString(argBstring);
+          Uart0_Send(' ');
+
           res = ExecucaoOperacao(operacao);
           OutputBuffRes64bitsHL(res);
-          if(flow_flags == DivPZero){
-            TransmiteOutpurBuff();
+          if(flow_flag != DivPZero){
+            TransmiteString(outputbuffer);
           } else {
-            TransmiteString(errostring);
+            TransmiteConstString(errostr);
+            
           }
-          acao = reinicia; 
+          flow_flag = Reinicia; 
           break;
-          
-      default :
-        break;
+
       }
-      
+        
       
        chaves = PortJ_Input();
        switch (chaves)
@@ -144,31 +197,20 @@ int main(void)
 }
 
 
-// executa o parsing da string
-void Parsestring(uint8_t n_op, uint8_t n_fim)
+
+
+int32_t ParseArgString(uint8_t *argstr)
 {
-  uint8_t cont = 0;
-  //int32_t arg = 0;
+  uint32_t result = 0;
+  uint16_t sinal = 1;
   
-  arg =0; //debug
-  while (cont < n_op)
-  {
-      arg = arg * 10; // multiplica por 10
-      arg = arg + (buffer[cont] - '0');
-      cont++;
+  if (*argstr == '-') sinal = -1;
+  while (*argstr != 0){
+    result = result*10;
+    result += *argstr++ - '0';
   }
-  operandoA = arg;
-  cont++; // pula o caractere do operador
-  arg = 0;
-  while (cont < n_fim)
-  {
-      arg = arg*10; // multiplica por 10
-      arg = arg + (buffer[cont] - '0');
-      cont++;
-  }
-  operandoB = arg;
   
-  return;
+  return result * sinal;
 }
 
 
@@ -189,9 +231,9 @@ int64_t ExecucaoOperacao (operacao_enum op)
     break;
   case divisao:
     if(operandoB == 0) {
-      erroflag = 1;
+      flow_flag = DivPZero;
     } else {
-      erroflag = 0;
+      flow_flag = OK;
       resultado = operandoA / operandoB;
     }
     break;
@@ -204,50 +246,12 @@ int64_t ExecucaoOperacao (operacao_enum op)
 
 
 
-void OutputBuffRes64bits(int64_t res)
-{       
-  uint8_t cont = 0;
-  uint64_t sub = 1000000000000000000;
-
-  
-  if(res <0) 
-  {
-    outputbuffer[n++] = '-';
-    res = -res;
-  }
-  if (res == 0)  
-  {
-     outputbuffer[n++] = '0';
-     outputbuffer[n++] = '\n';
-     outputbuffer[n++] = '\r';
-     return ;
-  }
-
-
-  while(res != 0){
-      
-    cont = 0;
-    while ( res - sub > 0)
-    {
-      res -= sub;
-      cont++;
-    }
-    
-    outputbuffer[n++] = '0'+ cont;
-    sub = sub / 10;
-  }
-  
-  return;
-}
-
 
 
 void OutputBuffRes64bitsHL(int64_t res)
 {       
   uint8_t cont = 0, n=0, m=0;
   uint8_t invstring[20]={0};
-
-  // outputbuffer[m++] = '='; //já tem um igual ecoado...
 
   if (res <0) 
   {
@@ -257,8 +261,6 @@ void OutputBuffRes64bitsHL(int64_t res)
   if (res == 0)  
   {
      outputbuffer[m++] = '0';
-//     outputbuffer[m++] = '\n'; // no reinicia
-//     outputbuffer[m++] = '\r';
      outputbuffer[m++] = 0;
      return ;
   }
@@ -273,10 +275,8 @@ void OutputBuffRes64bitsHL(int64_t res)
  
   while (n > 0)
     outputbuffer[m++] = invstring[--n];
-    
-//   outputbuffer[m++] = '\n';// no reinicia
-//   outputbuffer[m++] = '\r';
-   outputbuffer[m++] = 0;
+  
+  outputbuffer[m++] = 0;
   
   return;
 }
@@ -290,7 +290,15 @@ void TransmiteString(uint8_t *string )
   
 }
 
-void TransmiteOutpurBuff()
+void TransmiteConstString(const uint8_t *string )
+{
+//  uint8_t m = 0;
+  while (*string != 0 )
+    Uart0_Send( *string++);
+  
+}
+
+void TransmiteOutputBuff()
 {
   uint8_t m = 0;
   while (outputbuffer[m] != 0 )
